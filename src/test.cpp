7a0fe4ad56e221bf6bb7d705cf161b01b3a39b42
Stargate67 <66971848+Stargate67@users.noreply.github.com>
2024-08-12 23:10:01 +0200

    yes

--- src/test.cpp
+++ src/test.cpp
@@ -41,33 +41,46 @@
 // lines are arranged in columns, progressive order.  The shield uses
 // 800 KHz (v2) pixels that expect GRB color data.
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
-  NEO_MATRIX_BOTTOM  + NEO_MATRIX_LEFT +
-  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+  NEO_MATRIX_BOTTOM  + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800);
 
-const uint16_t colors[] = {
-  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+const uint16_t colors[] = { matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
 
+bool bSerialDebug = false;
 String sPrintToMatrix;
 unsigned long currmillis = 0; //used in my function to find the current millis()
 unsigned long prevmillis = 0; //used to hold previous value of currmillis
+unsigned long prevmillis1 = 0; //used to hold previous value of currmillis
 int boolval = 0;    //used to control whether to write the brightness value to the led or not
 int timer = 0;     //used in the delay function, difference between currmillis and prevmillis
 int i;
 struct tm timeinfo;
 int  pixelPerChar = 6;
 int  maxDisplacement;
+int iState = 0;
 String sPrintdate;
 String sPrintShortdate;
 String sPrintClock;
 String sLocalIP;
 String sTempExt;
+String sTrend;
+float rAvgTempExt = 0;
+float rTmp = 0;
 unsigned long time_now = 0;
 unsigned long time1_now = 0;
 
+const int numReadings = 10;
+float readings [numReadings];
+int iReadIndex = 0;
+int iStartIndex = 0;
+float total = 0;
+int aisVal = 0;
+
 WiFiClient client;
 ModbusIP mb;  //ModbusIP object
-IPAddress MBremote(192, 168, 0, 105);  // Address of Modbus Slave device
+//IPAddress MBremote(192, 168, 0, 105);  // Address of Modbus Slave device
+IPAddress MBremote(77,204,15,6);  // Address of Internet Box 
 const int START_REG = 12689;           // Starting holding register
 const int NUM_REGS = 10;           // Number of holding registers to read
 const int INTERVAL = 5000;         // Interval between reads (in milliseconds)
@@ -75,37 +88,127 @@ const int INTERVAL = 5000;         // Interval between reads (in milliseconds)
 uint16_t MBresult[NUM_REGS];
 uint8_t show = NUM_REGS;  // Counter for displaying values
 uint32_t LastModbusRequest = 0;  // Variable to track the last Modbus request time
-float rTemp_Ext;
+float rTempExt;
+
+
+float smooth(float fInput) { /* function smooth */
+  //Perform average on sensor readings
+  float average;
+  // subtract the last reading:
+  total = total - readings[iReadIndex];
+  // read the sensor:
+  readings[iReadIndex] = fInput;
+  // add value to total:
+  total = total + readings[iReadIndex];
+  // handle index
+  iReadIndex = iReadIndex + 1;
+  if (iReadIndex >= numReadings) {
+    iReadIndex = 0;
+  }
+  if (iStartIndex < numReadings) {
+    iStartIndex = iStartIndex + 1;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+
+  if (iStartIndex >= numReadings) {
+    return average;
+  } else {
+    return fInput;
+  }
+}
 
 void ReadModbus() {
+  mb.task();
   if (mb.isConnected(MBremote)) {  
-    // Read holding registers from Modbus Slave
-    uint8_t transaction = mb.readHreg(MBremote, START_REG, MBresult, NUM_REGS, nullptr, 1);
-    
-    // Wait for the transaction to complete
-    while(mb.isTransaction(transaction)) {
-      mb.task();
-      delay(10);
-    }
+    if (bSerialDebug) Serial.println(String(iState));
+    switch (iState)
+      {
+      case 0:
+      {
+        // Read holding registers from Modbus Slave
+        uint8_t transaction = mb.readHreg(MBremote, START_REG, MBresult, NUM_REGS, nullptr, 1);        
+        //if (mb.isTransaction(transaction)) {
+          prevmillis1 = millis();
+          iState = 10;
+        ///} else {
+        //  mb.disconnect(MBremote);
+        //}
+       } 
+       break;
+      case 10:
+      {
+        // Wait for the transaction to complete
+        if (millis() >= prevmillis1 + 50){ //Process MB client request each second
+          prevmillis1 = millis();
+//          mb.task();
+          iState = 20;
+        }
+      }
+      break;
+      case 20:
+      {
+        mb.disconnect(MBremote);
 
-    rTemp_Ext = (MBresult[7] * 100.0 / 32764.0) - 50.0; // Mise a l'echelle 
-    Serial.print("T ext. = ");
-    Serial.println(rTemp_Ext);
-    sTempExt = "T ext: " + String(rTemp_Ext) + " C";
+        rTmp = (MBresult[7] * 100.0 / 32764.0) - 50.0; // Mise a l'echelle
+        rTempExt = round(rTmp * 100.0)/100.0;
+        //if (bSerialDebug) Serial.print("T.Ext. = ");
+        //if (bSerialDebug) Serial.println(rTempExt);
+        sTempExt = "T.Ext:" + String(rTempExt) + " C";
+        // Calcul la moyenne 
+        rAvgTempExt = round(smooth(rTempExt)*100.0)/100.0;
+        sTrend = String(" =");
+        if (rTempExt > rAvgTempExt) {
+          sTrend = String(" Up");
+        } else if (rTempExt < rAvgTempExt){
+          //sTrend = String(" Dn");
+          sTrend = String(" Dn");
+        }
 
-    /*
-    // Print holding register values
-    Serial.println("Holding Register Values:");
-    for (int i = 0; i < NUM_REGS; i++) {
-      Serial.print("Register ");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.println(MBresult[i]);
-    }
-    */
+        if (bSerialDebug) { 
+          Serial.print("T.Ext. = ");
+          Serial.println(String(rTempExt));
+          Serial.print("Avg T.Ext. = ");
+          Serial.print(String(rAvgTempExt));
+          Serial.println(sTrend);
+        }
+        LastModbusRequest = millis();
+        
+        iState = 30;
+        
+        if (bSerialDebug) {
+        // Print holding register values
+          Serial.println("Holding Register Values:");
+          for (int i = 0; i < NUM_REGS; i++) {
+            Serial.print("Register ");
+            Serial.print(i);
+            Serial.print(": ");
+            Serial.println(MBresult[i]);
+          }
+        }
+      }
+      break;
+      case 30:
+      {     // Wait 5 sec
+        if (millis() - LastModbusRequest >= INTERVAL) {
+          LastModbusRequest = millis();
+          iState = 0;
+        }
+      }
+      break;
+      default:
+      {
+        iState = 0;
+      }
+      break;
+      }
+
   } else {
     // If not connected, try to connect
     mb.connect(MBremote);
+
+    //sTempExt = "ModBus Connecting to " + String(MBremote);
   }
 }
 
@@ -117,7 +220,7 @@ void timeloop (int interval){ // the delay function
 }
 
 void setTimezone(String timezone){
-  Serial.printf("  Setting Timezone to %s\n",timezone.c_str());
+  if (bSerialDebug) Serial.printf(" Setting Timezone to %s\n",timezone.c_str());
   setenv("TZ",timezone.c_str(),1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
   tzset();
 }
@@ -125,13 +228,13 @@ void setTimezone(String timezone){
 void initTime(String timezone){
   struct tm timeinfo;
 
-  Serial.println("Setting up time");
+  if (bSerialDebug) Serial.println("Setting up time");
   configTime(0, 0, "pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
   if (!getLocalTime(&timeinfo)) {
-    Serial.println("  Failed to obtain time");
+    if (bSerialDebug) Serial.println(" Failed to obtain time");
     return;
   }
-  Serial.println("  Got the time from NTP");
+  if (bSerialDebug) Serial.println(" Got the time from NTP");
   // Now we can set the real timezone
   setTimezone(timezone);
 }
@@ -141,12 +244,12 @@ void printLocalTime(){
   if (!getLocalTime(&timeinfo)) {
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S zone %Z %z ");
+  if (bSerialDebug) Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S zone %Z %z ");
 }
 
 void startWifi(){
   WiFi.begin(ssid, password);
-  //Serial.println("Connecting Wifi");
+  //if (bSerialDebug) Serial.println("Connecting Wifi");
   //DisplayScrollingString(green, "Connecting Wifi");
   while (WiFi.status() != WL_CONNECTED) {
     //DisplayScrollingString(green, ".");
@@ -171,14 +274,13 @@ void setTime(int yr, int month, int mday, int hr, int minute, int sec, int isDst
   tm.tm_sec = sec;
   tm.tm_isdst = isDst;  // 1 or 0
   time_t t = mktime(&tm);
-  Serial.printf("Setting time: %s", asctime(&tm));
+  if (bSerialDebug) Serial.printf("Setting time: %s", asctime(&tm));
   struct timeval now = { .tv_sec = t };
   settimeofday(&now, NULL);
 }
 
 void setup() {  
-  Serial.begin(115200);
-  //Serial.setDebugOutput(true);
+  if (bSerialDebug) Serial.begin(115200);
 
   startWifi();
   //FadeString(amber, "Sync NTP");
@@ -214,7 +316,7 @@ void loop() {
 
       sPrintShortdate = (String(timeinfo.tm_mday).length() > 1 ? String(timeinfo.tm_mday) : "0" + String(timeinfo.tm_mday)) + "/" + (String(timeinfo.tm_mon+1).length() > 1 ? String(timeinfo.tm_mon+1) : "0" + String(timeinfo.tm_mon+1));
 
-      //Serial.println(sPrintdate);
+      //if (bSerialDebug) Serial.println(sPrintdate);
       i = timeinfo.tm_min % 2;
   }
 
@@ -229,9 +331,9 @@ void loop() {
     sLocalIP = "";
   }
 
-  sPrintToMatrix =sLocalIP + " " + sPrintShortdate + " " + sPrintClock + " " + sTempExt;
+  sPrintToMatrix =sLocalIP + " " + sPrintShortdate + " " + sPrintClock + " " + sTempExt + sTrend;
 
-  Serial.println(sPrintToMatrix);
+  //if (bSerialDebug) Serial.println(sPrintToMatrix);
   matrix.print(sPrintToMatrix);
 
   //maxDisplacement = sPrintToMatrix.length() * pixelPerChar + matrix.width();
@@ -246,10 +348,8 @@ void loop() {
   }
 
   matrix.show();
-  timeloop(90);
-
-  if (millis() - LastModbusRequest >= INTERVAL) {
-    LastModbusRequest = millis();
-    ReadModbus();
-  }
+  //timeloop(70);
+  delay(50);
+  
+  ReadModbus();
 }
